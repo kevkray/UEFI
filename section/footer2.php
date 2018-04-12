01c5c54742057817e8ba035aabdff379457b7ee6
kevkray <kevindiaz9@outlook.com>
2018-04-12 09:55:13 -0500

    verion kevin footer 1

---
+++ section/footer2.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+</head>
+<body>
+<!--Pie de página-->
+    <footer id="footer">
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-4">
+                <h5>Unidad de Extensión de la facultad de Ingeniería de la Universidad Distrital Francisco José de Caldas</h5>
+                    <ul class="s-footer__linklist">
+                        <li>
+                            <p>Institución de Educación Superior sujeta a inspección y vigilancia por el Ministerio de Educación Nacional</p>
+                        </li>
+                        <li>
+                        <p>NIT. 899.999.230.7</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                <h4>Enlaces</h4>
+                    <ul class="s-footer__linklist">
+                        <li>
+                            <a href="index.html">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="biografia.html">Biografía</a>
+                        </li>
+                        <li>
+                            <a href="videos.html">Videos</a>
+                        </li>
+                        <li>
+                            <a href="musica.html">Música</a>
+                        </li>
+                        <li>
+                            <a href="educacion.html">Educación</a>
+                        </li>
+                        <li>
+                            <a href="contacto.html">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                <h4>Enlaces</h4>
+                    <ul class="s-footer__linklist">
+                        <li>
+                            <a href="index.html">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="biografia.html">Biografía</a>
+                        </li>
+                        <li>
+                            <a href="videos.html">Videos</a>
+                        </li>
+                        <li>
+                            <a href="musica.html">Música</a>
+                        </li>
+                        <li>
+                            <a href="educacion.html">Educación</a>
+                        </li>
+                        <li>
+                            <a href="contacto.html">Contacto</a>
+                        </li>
+                    </ul>
+    			</div>
+    		</div>
+            <div class="row">
+    			<div class="col-md-12 align-self-center">
+					
+    			</div>
+    		</div>
+            <div class="row">
+    			<div class="col-md-12 align-self-center">
+					
+    			</div>
+    		</div>
+    	</div>
+    </footer>
+  <!-- #Fin Pie de página -->
+</body>
+</html>
