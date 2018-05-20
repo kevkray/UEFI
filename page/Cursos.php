ff83a9faaac93ce810c48dc3d57777301d91cf13
kevkray <kevindiaz9@outlook.com>
2018-05-20 10:53:03 -0500

    actualización de la conexión v1

--- page/Cursos.php
+++ page/Cursos.php
@@ -13,6 +13,34 @@
 	<!-- /Hojas de estilo -->
 </head>
 <body>
+	<!--Código de consultas a la BD-->
+	<?php include '../consultas/conexion.php';
+		function generarlistaCursos() {
+			$conexion=mysqli_connect("localhost","root","","bduefi") or
+			die("Problemas con la conexión");
+			//pa es programaacademico
+			//tpa es tipoprogramaacademico
+			$registros=mysqli_query($conexion,"select pa.nomprogramaacademico, pa.peqdescripprogaca from programaacademico pa where pa.idtipoprogramaacademico=1") or die("Problemas en el select:".mysqli_error($conexion));
+			$numeroRegistros = mysqli_num_rows($registros);
+			$contador=0;
+			if ($numeroRegistros!=0) {
+				while ($reg=mysqli_fetch_array($registros)){
+					$nombrecurso = $reg['nomprogramaacademico'];
+					$descurso = $reg['peqdescripprogaca'];
+					//echo "<div role=\"tabpanel\" class=\"tab-pane fade\" id=".$nombrecurso.">";
+					//echo "<div role=\"tabpanel\" class=\"tab-pane fade\" id=".$descurso.">";
+				
+				}
+				mysqli_close($conexion);
+			}else{
+				$mensajeCursos = "No hay cursos actualmente";
+				echo "<h3 class=\"text-center text-muted\">".$mensajeCursos."</h3>";
+			}
+		}
+
+
+	?>
+	<!--Código de consultas a la BD-->
 
 	<section id="inicio" class="sticky-top">
 	<!-- Header -->
@@ -45,13 +73,13 @@
 						<a class="nav-link active dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Educación</a>
 						<!-- Dropdown -->
 						<div class="dropdown-menu navbar-dark">
-							<a href="../page/Cursos.php" class="dropdown-item font-weight-bold">Cursos</a>
+							<a href="../page/Cursos.php" class="dropdown-item font-weight-bold">Cursos</a> 
 							<a href="../page/Diplomados.php" class="dropdown-item font-weight-bold">Diplomados</a>
 						</div>
 						<!-- /Dropdown -->
 					</li> 
 					<li class="nav-item pr-2">
-						<a class="nav-link" href="../page/Conocenos.php">Conócenos</a>
+						<a class="nav-link" href="../page/Conocenos.php">Conocenos</a>
 					</li>
 					<li class="nav-item pr-2">
 						<a class="nav-link" href="../page/Convenios.php">Convenios</a>
@@ -85,93 +113,11 @@
 			<!-- Cursos en programación -->
 			<div role="tabpanel" class="tab-pane fade in active" id="Programacion">
 				<div class="row">
+				<?php generarlistaCursos();?>
 					<div class="col-lg-3 col-sm-3 ">
 						<!-- Thumbnails container -->
 						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-6.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 2</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
-							<img src="../img/portfolio-7.jpg" width=100% alt="">
-							<div class="caption">
-								<h3 class="text-secondary">Curso 1</h3>
-								<p class="text-secondary">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-								<p class="text-secondary"><a href="../page/Programa_Especifico.php" class="btn btn-primary">Action</a></p>
-							</div>
-						</div>
-						<!-- /Thumbnails container -->
-					</div>
-					<div class="col-lg-3 col-sm-3 ">
-						<!-- Thumbnails container -->
-						<div class="thumbnail">
+	
 							<img src="../img/portfolio-7.jpg" width=100% alt="">
 							<div class="caption">
 								<h3 class="text-secondary">Curso 1</h3>
@@ -214,7 +160,7 @@
 				</div>
 			</div>
 			<!-- /Cursos en inscripción -->
-			<!-- Cursos en progreso -->
+			<!-- Cursos en dictando -->
 			<div role="tabpanel" class="tab-pane fade" id="Dictando">
 				<div class="row">
 				<div class="col-lg-3 col-sm-3 ">
